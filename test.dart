167a3af33345ae6db2eadadc55527ba383378c02
dev rai <devdavinchi06@gmail.com>
2025-10-26 22:53:44 +0545

    switch case in dart

--- test.dart
+++ test.dart
@@ -1,19 +1,15 @@
-//function that returns true if the number is even and false if odd .
-ddd(int number) {
-  if (number > 0) {
-    return "positive+";
-  } else if (number < 0) {
-    return "negative";
-  } else if (number == 0) {
-    return "zero";
-  } else {
-    return "invalid";
-  }
-}
-
 void main() {
-  print(ddd(5));
-  print(ddd(-6));
-  print(ddd(0));
-  print(ddd(-0));
+  String days = "b";
+  switch (days) {
+    case "a":
+      print("sunday");
+      break;
+    case "b":
+      print("monday");
+      break;
+    case "c":
+      print("tuesday");
+      break;
+    default:
+  }
 }
