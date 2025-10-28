2d2c736c0a6cb753faead0975ee8f14e4460a7c8
dev rai <devdavinchi06@gmail.com>
2025-10-28 19:20:43 +0545

    enumerations 3rd

--- test.dart
+++ test.dart
@@ -1,11 +1,23 @@
 /////////dart emunerations
 ///
-enum Check { firstNaame }
+enum Ani { cat, dog, bunny, pup }
 
 void main() {
-  void test() {
-    print(Check.firstNaame);
+  void test(Ani Animal) {
+    switch (Animal) {
+      case Ani.cat:
+        print("this cat is cattying");
+        break;
+      case Ani.dog:
+        print("this dog is doggying ");
+        break;
+      case Ani.pup:
+        print("this pup is pupping ");
+        break;
+      default:
+        print("unknown animal");
+    }
   }
 
-  test();
+  test(Ani.dog);
 }
